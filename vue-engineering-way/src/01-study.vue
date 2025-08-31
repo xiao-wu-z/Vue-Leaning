8e0dd9c528625b2acb4839ea9506c866bb952ea5
xiao-wu-z <2721059902@qq.com>
2025-08-31 21:10:55 +0800

    Leaning

---
+++ vue-engineering-way/src/01-study.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import { ref } from 'vue'
+  const msg = ref("Hello World!")
+  const src = ref('<span style="color:red;">Hello Vue3+Vite</span>')
+  const url = ref('https://www.baidu.com/')
+  const count = ref(0)
+  const addOne = () => {
+    count.value++
+  }
+  const addThree = (num) => {
+    count.value+=num
+  }
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <div v-html="src"></div>
+  <a v-bind:href="url">百度一下</a>
+  <div>{{ count }}</div>
+  <button v-on:click="count++">+1</button>
+  <button @click="addOne">+1</button>
+  <button @click="addThree(3)">+3</button>
+</template>
+
+<style scoped>
+
+</style>
--- vue-engineering-way/src/App.vue
+++ vue-engineering-way/src/App.vue
@@ -1,11 +1,50 @@
 <script setup>
-  const msg = "Hello World!"
+    
 </script>
 
+
 <template>
-  <h1>{{ msg }}</h1>
+<div class='bit-banner'>
+  <img src="https://bit-1256306791.cos.ap-chengdu.myqcloud.com/fffa10ace4b071da45d9a0c4.jpg" 
+  alt="比特轮播图" />
+
+  <div class="ctrl">
+    <a href="javascript:;" class="btn pre">&lt;</a>
+
+    <a href="javascript:;" class="btn next">&gt;</a>
+  </div>
+
+</div>
 </template>
+
 
 <style scoped>
 
+  .bit-banner {
+    position: relative;
+    width: 1200px;
+    height: 337px;
+    margin: 150px auto;
+  }
+
+  .bit-banner .ctrl .btn {
+		position: absolute;
+		top: 50%;
+		width: 30px;
+		height: 30px;
+		margin-top: -15px;
+		font-size: 14px;
+		line-height: 30px;
+		text-align: center;
+		border-radius: 50%;
+		background: rgba(0, 0, 0, 0.3);
+	}
+
+  .bit-banner .ctrl .btn.prev {
+		left: 15px;
+	}
+
+	.bit-banner .ctrl .btn.next {
+		right: 15px;
+	}
 </style>
