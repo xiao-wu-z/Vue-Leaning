58748a842b2d931bc22c36cc7a21d5e4b8275dbd
xiao-wu-z <2721059902@qq.com>
2025-09-02 20:33:52 +0800

    Leaning

---
+++ vue-engineering-way/src/04-橱窗.vue
@@ -0,0 +1,66 @@
+
+<script setup>
+import { ref } from 'vue';
+
+const goodsList = ref([
+		{ id: 101, name: 'biangbiang面', price: 13 },
+		{ id: 102, name: '葫芦头', price: 20 },
+		{ id: 103, name: '冰峰', price: 2 },
+		{ id: 104, name: '肉夹馍', price: 11 },
+		{ id: 105, name: '羊肉泡馍', price: 18 }
+	])
+
+    const del = (i) => {
+      if (window.confirm("确认删除吗？")) {
+        goodsList.value.splice(i,1)
+      }
+    }
+</script>
+
+<template>
+<table border="1" cellspacing="0" cellpadding="0">
+  <caption>
+    <h3>橱窗</h3>
+  </caption>
+  <thead>
+    <tr>
+      <th>商品名称</th>
+      <th>商品价值</th>
+      <th>操作</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(item, index) in goodsList">
+      <th>{{ item.name }}</th>
+      <th>{{ item.price }}</th>
+      <th>
+        <button @click="del(index)">删除</button>
+      </th>
+    </tr>
+  </tbody>
+  <tfoot v-if="goodsList.length === 0">
+			<tr>
+				<td colspan="3">暂无数据啦</td>
+			</tr>
+		</tfoot>
+</table>
+</template>
+
+
+<style lang="scss">
+table{
+  width: 500px;
+  margin: 150px auto;
+  text-align: center;
+  font-family: 'Courier New', Courier, monospace;
+
+		tr {
+			height: 40px;
+		}
+
+		tfoot {
+			height: 100px;
+		}
+}
+
+</style>
--- vue-engineering-way/src/App.vue
+++ vue-engineering-way/src/App.vue
@@ -1,66 +1,62 @@
-
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
+import './style/index.css'
 
-const goodsList = ref([
-		{ id: 101, name: 'biangbiang面', price: 13 },
-		{ id: 102, name: '葫芦头', price: 20 },
-		{ id: 103, name: '冰峰', price: 2 },
-		{ id: 104, name: '肉夹馍', price: 11 },
-		{ id: 105, name: '羊肉泡馍', price: 18 }
-	])
+const todoList = ref([
+  { id: 321, name: '吃饭', finished: false },
+  { id: 666, name: '睡觉', finished: true },
+  { id: 195, name: '打⾖⾖', finished: false },
+])
 
-    const del = (i) => {
-      if (window.confirm("确认删除吗？")) {
-        goodsList.value.splice(i,1)
-      }
-    }
-</script>
+const title = ref('')
 
-<template>
-<table border="1" cellspacing="0" cellpadding="0">
-  <caption>
-    <h3>橱窗</h3>
-  </caption>
-  <thead>
-    <tr>
-      <th>商品名称</th>
-      <th>商品价值</th>
-      <th>操作</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="(item, index) in goodsList">
-      <th>{{ item.name }}</th>
-      <th>{{ item.price }}</th>
-      <th>
-        <button @click="del(index)">删除</button>
-      </th>
-    </tr>
-  </tbody>
-  <tfoot v-if="goodsList.length === 0">
-			<tr>
-				<td colspan="3">暂无数据啦</td>
-			</tr>
-		</tfoot>
-</table>
-</template>
+const onAdd = () => {
+  const name = title.value.trim()
 
+  if (!name) return alert("名称不能为空!") 
 
-<style lang="scss">
-table{
-  width: 500px;
-  margin: 150px auto;
-  text-align: center;
-  font-family: 'Courier New', Courier, monospace;
-
-		tr {
-			height: 40px;
-		}
-
-		tfoot {
-			height: 100px;
-		}
+  todoList.value.push({
+    name,
+    id: Date.now(),
+    finished: false
+  })
 }
 
+const del = (index) => {
+  if (window.confirm("确认删除吗?")) {
+    todoList.value.splice(index, 1)
+  }
+}
+
+const onClear = () => {
+  todoList.value = []
+}
+</script>
+
+
+<template>
+  <section clsss="todoapp">
+    <header class="header">
+      <h1>⽐特⼈记事本</h1>
+      <input placeholder="请输⼊任务" class="new-todo" v-model="title"/>
+      <button class="add" @click="onAdd">添加任务</button>
+    </header>
+    <section class="main">
+      <ul class="todo-list">
+        <li class="todo" v-for="(item, index) in todoList" :key="item.id">
+          <div class="view">
+            <span class="index">{{ index+1 }}</span> <label> {{ item.name }} </label>
+            <button class="destroy" @click="del(index)"></button>
+          </div>
+        </li>
+      </ul>
+    </section>
+    <footer class="footer">
+      <span class="todo-count"> 合 计 : <strong> 0 </strong></span>
+      <button class="clear-completed" @click="onClear">清空任务</button>
+    </footer>
+  </section>
+</template>
+
+<style scoped>
 </style>
